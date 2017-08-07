687643dba1ffb03c7717fc32a410e413836ff186
kylearora <kylearora@gmail.com>
2017-08-07 15:59:48 -0500

    Final Commit

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-let container = document.querySelector('.container')
-let main = document.querySelector('.main')
-let basics = document.querySelector('.basics')
 let info = document.querySelector('.info')
-let story = document.querySelector('.story')
 let bio = document.querySelector('.bio')
 let photo = document.querySelector('.photo')
 
-// 2. Create our Ajax Request
   let req = new XMLHttpRequest();
   req.open("GET", "https://api.github.com/users/kylearora");
   req.addEventListener('load', function(){
@@ -27,19 +22,3 @@ let photo = document.querySelector('.photo')
       `;
 })
 req.send()
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let li = document.createElement("li");
-// li.innerHTML = (data.results[i].name);
-// characters.append(li)
