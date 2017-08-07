be0e1398387f4e1dced7a1476c8ab1ee616590c7
kylearora <kylearora@gmail.com>
2017-08-07 16:01:54 -0500

    Final Commit

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ let photo = document.querySelector('.photo')
     let data = JSON.parse(this.responseText);
       console.log(data)
 
-      bio.innerHTML = `${data.bio}`
-      photo.innerHTML = `<img src=${data.avatar_url}>`;
-
       info.innerHTML = `
 
-    <span class="infoLabels">Name:</span>  ${data.name} <br>
-    <span class="infoLabels">GitHub URL:</span> <a href="${data.html_url}">${data.login}</a><br>
-    <span class="infoLabels">Email:</span> <a href="mail.to:kylearora@gmail.com">kylearora@gmail.com</a><br>
-    <span class="infoLabels">Company:</span>  ${data.company} <br>
-    <span class="infoLabels">Website:</span> <a href="${data.blog}">kylearora.com</a>
+        <span class="infoLabels">Name:</span>  ${data.name} <br>
+        <span class="infoLabels">GitHub URL:</span> <a href="${data.html_url}">${data.login}</a><br>
+        <span class="infoLabels">Email:</span> <a href="mail.to:kylearora@gmail.com">kylearora@gmail.com</a><br>
+        <span class="infoLabels">Company:</span>  ${data.company} <br>
+        <span class="infoLabels">Website:</span> <a href="${data.blog}">kylearora.com</a>
 
-      `;
+        `;
+
+        bio.innerHTML = `${data.bio}`
+        photo.innerHTML = `<img src=${data.avatar_url}>`;
 })
 req.send()
